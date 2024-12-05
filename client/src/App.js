c5860fef83b479ad16042837ea329587df206904
Breeann <breeann.doremus@spaceforce.mil>
2024-12-04 21:02:27 -0500

    routes and homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-
-
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
   return (
-  <div>Hello Yall</div>
+    <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/items" element={<Items />} />
+      <Route path="/itesm/:id" element={<Item />} />
+    </Routes>
+  </Router>
 )}
 
 export default App;
